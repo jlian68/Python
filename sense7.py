1195662857b9ec36e322cb7027edb5265d268b5f
jlian68 <std135463@ac.eap.gr>
2026-03-12 10:59:07 +0200

    new sense7

--- sense7.py
+++ sense7.py
@@ -1,26 +1,33 @@
-from sense_emu import SenseHat
+from sense_hat import SenseHat
 from time import sleep
+import sys
+
 s = SenseHat()
 s.low_light=True
 
 # Ορίζουμε τις συναρτήσεις:
-def pano():
-  s.show_letter('P')
+def up():
+  s.show_letter('U')
 
-def kato():
-  s.show_letter('K')
-
-def aristero():
-  s.show_letter('A')
-  
-def deksi():
+def down():
   s.show_letter('D')
 
-while True:
-    s.stick.direction_up = pano
-    s.stick.direction_down = kato
-    s.stick.direction_left = aristero
-    s.stick.direction_right = deksi
-    # με το μεσαίο κουμπί εκτελείται η μέθοδος s.clear
-    s.stick.direction_middle = s.clear
-    sleep(0.1)
+def left():
+  s.show_letter('L')
+  
+def right():
+  s.show_letter('R')
+
+try:
+    while True:
+        s.stick.direction_up = up
+        s.stick.direction_down = down
+        s.stick.direction_left = left
+        s.stick.direction_right = right
+        # με το μεσαίο κουμπί εκτελείται η μέθοδος s.clear
+        s.stick.direction_middle = s.clear
+        sleep(0.1)
+except KeyboardInterrupt:
+    s.clear()
+    print('\b\bExiting...')
+    sys.exit(0)
